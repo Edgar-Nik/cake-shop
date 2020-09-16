a4ff3b16cfc79e56388d52989f1f6908ef06f195
edgar <edgar.nikogosian98@gmail.com>
2020-09-16 22:30:45 +0400

    starting pos

---
+++ src/components/header/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './header.module.css';
+
+function Header() {
+  return (
+    <div className="App">
+      <header >
+      
+      </header>
+    </div>
+  );
+}
+
+export default Header;
